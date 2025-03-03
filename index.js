0fbe76bef3835d709f671bd629c851fb2895d277
xaxoman <dennisx.fjdv8@silomails.com>
2025-03-03 15:02:27 +0100

    Finished the project, the backend correctly fetches the hours and indicates the current time.

--- index.js
+++ index.js
@@ -4,6 +4,39 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+/* We're going to use the aladhan API
+
+    API doc: https://aladhan.com/prayer-times-api
+*/
+
+app.get('/prayers', async (req, res) => {
+
+    let {city, country}  = req.query;
+
+    try {
+
+        const response = await fetch(`http://api.aladhan.com/v1/timingsByCity?city=${city}&country=${country}&method=2`);
+        
+        if (!response.ok) {
+            throw new Error('Problema nel fetch degli orari..');
+        }
+
+        const data = await response.json();
+
+        // La data in calendario gregoriano e quella in calendario islamico
+        const responseData = {
+            timings: data.data.timings,
+            date: `${data.data.date.readable} | ${data.data.date.hijri.date} ${data.data.date.hijri.month.en} ${data.data.date.hijri.year}`
+        };
+
+        res.json(responseData);
+        
+    } catch (error) {
+        res.status(404).json({ error: error.message });
+    }
+})
+
+
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
